f052578c4b69b0c780d5787dd7a557e079c9e2d2
Nacho Coloma <icoloma@gmail.com>
2022-09-14 22:03:47 +0200

    First revision completed

--- src/_js/youtube.ts
+++ src/_js/youtube.ts
@@ -1,18 +1,15 @@
-const youtubeVideos = document.querySelectorAll(".video-preview-hover");
+const youtubeVideos = document.querySelectorAll(".video-link");
 Array.prototype.forEach.call(youtubeVideos, function (item: HTMLElement) {
   item.addEventListener("click", function (e) {
     try {
-      const currentTarget: HTMLElement | null = e.currentTarget as HTMLElement;
-      const parent = currentTarget?.parentElement;
-      if (parent && !parent.classList.contains("playing")) {
-        const dataset = currentTarget?.dataset;
+      const link: HTMLElement | null = e.currentTarget as HTMLElement;
+      if (link && !link.classList.contains("playing")) {
+        const dataset = link?.dataset;
         const id = dataset.id;
         const title = dataset.title;
-        const iframe = `<iframe title="${title}" class="video-iframe" width="560" height="315" src="https://www.youtube-nocookie.com/embed/${id}?autoplay=1&modestbranding=1&rel=0&wmode=transparent" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-        parent.classList.add("playing");
-        const container = document.createElement("div");
-        container.innerHTML = iframe;
-        parent.appendChild(container.firstElementChild as Node);
+        const iframe = `<iframe title="${title}" class="video-iframe"  src="https://www.youtube-nocookie.com/embed/${id}?autoplay=1&modestbranding=1&rel=0&wmode=transparent" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+        link.classList.add("playing");
+        link.innerHTML = iframe;
       }
       e.preventDefault();
     } catch (error) {}
