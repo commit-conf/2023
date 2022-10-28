f89132f90fc93b268877bf869251b30f474161c5
Carlos Coloma <ccescribano@gmail.com>
2022-10-28 13:57:43 +0100

    Edit C4P URL

--- src/_data/site.js
+++ src/_data/site.js
@@ -10,8 +10,8 @@ module.exports = function () {
     twitter: "@commitconf",
     youtubeChannel: "UCd_1KHg4t2VKGsSDF8OD5Cw",
     environment: process.env.ENV || "prod",
-    c4pURL: "https://www.koliseo.com/commit/2023/sessions",
-    ticketsURL: "https://www.koliseo.com/commit/2023/tickets",
+    c4pURL: "https://koliseo.com/events/commit-2023/sessions",
+    ticketsURL: "https://koliseo.com/events/commit-2023/tickets",
     newsletter:
       "https://commit-conf.us8.list-manage.com/subscribe?u=b180f87ed019c243f111b693a&amp;id=6a6bdcb448",
     communities,
