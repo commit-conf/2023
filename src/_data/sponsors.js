cff2814855b5d60940521647adc0d151a04855bf
Carlos Coloma <ccescribano@gmail.com>
2023-01-23 15:50:24 +0000

    Add 2 sponsors

--- src/_data/sponsors.js
+++ src/_data/sponsors.js
@@ -1,1 +1,21 @@
-module.exports = [];
+module.exports = [
+  {
+    name: "Diamond",
+    items: [
+      {
+        name: "Universidad CEU San Pablo",
+        href: "http://www.uspceu.com/",
+      },
+    ],
+  },
+  {
+    name: "Gold",
+    items: [
+      {
+        name: "Autentia",
+        href: "https://www.autentia.com/",
+        svg: "true",
+      },
+    ],
+  },
+];
