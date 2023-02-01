2f9b6be9229704f2dd224724d6ec49b5fda86ac5
Laura Vignali <lvignali@extrema-sistemas.com>
2023-02-01 12:07:40 +0000

    Add agenda on website

--- src/_data/site.js
+++ src/_data/site.js
@@ -10,7 +10,7 @@ module.exports = function () {
     twitter: "@commitconf",
     youtubeChannel: "UCd_1KHg4t2VKGsSDF8OD5Cw",
     environment: process.env.ENV || "prod",
-    c4pURL: "https://koliseo.com/events/commit-2023/sessions",
+    c4pURL: "https://koliseo.com/events/commit-2023/agenda",
     ticketsURL: "https://koliseo.com/events/commit-2023/tickets",
     newsletter:
       "https://commit-conf.us8.list-manage.com/subscribe?u=b180f87ed019c243f111b693a&amp;id=6a6bdcb448",
